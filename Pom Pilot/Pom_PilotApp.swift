b6156dac1d94ec0c6e2589e3c35bb51b95c45aba
Walter de Bruijn <walter.debruijn@me.com>
2025-11-07 21:35:07 +0100

    Initial Commit

---
+++ Pom Pilot/Pom_PilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pom_PilotApp.swift
+//  Pom Pilot
+//
+//  Created by Walter de Bruijn on 07/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Pom_PilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
