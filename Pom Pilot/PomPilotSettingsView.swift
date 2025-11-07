8a26b0f312eafb4accdb2b5dc89abe2a25973636
Walter de Bruijn <walter.debruijn@me.com>
2025-11-07 22:17:10 +0100

    General app, general view and settings view. Missing configuration sheet that connects with main app.

---
+++ Pom Pilot/PomPilotSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  PomPilotSettingsView.swift
+//  Pom Pilot
+//
+//  Created by Walter de Bruijn on 07/11/2025.
+//
+
+import SwiftUI
+
+struct PomPilotSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PomPilotSettingsView()
+}
---
+++ Pom Pilot/PomPilotView.swift
@@ -0,0 +1,18 @@
+//
+//  PomPilotView.swift
+//  Pom Pilot
+//
+//  Created by Walter de Bruijn on 07/11/2025.
+//
+
+import SwiftUI
+
+struct PomPilotView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PomPilotView()
+}
